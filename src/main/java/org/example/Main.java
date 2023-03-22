055abced069028c2ab7e3645aada81ac7e58c832
Alex <alexsa-8@mail.ru>
2023-03-22 23:20:37 +0300

    Билет 13. Решение задачи.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Билет 13. Задача.
+        //Дан класс User, который содержит поля с именем и возрастом.
+        // Необходимо реализовать следующий метод:
+
+        //Задание №1 На входе коллекция пользователей
+        User user1 = new User("Ivan", 15);
+        User user2 = new User("Ivan", 15);
+        User user3 = new User("Petr", 35);
+        User user4 = new User("Petr", 35);
+        User user5 = new User("Roma", 25);
+        User user6 = new User("Oleg", 20);
+        User user7 = new User("Roma", 35);
+        List<User> userList = Arrays.asList(user1, user2, user3, user4, user5, user6, user7);
+        System.out.println("Коллекция пользователей:");
+        System.out.println(userList);
+
+        //Задание №2 Необходимо удалить все дубли (одинаковое имя и возраст)
+        List<User> userList1 = new ArrayList<>();
+        for (User user : userList) {
+            if (!userList1.contains(user)) {
+                userList1.add(user);
+            }
+        }
+        System.out.println("Коллекция пользователей с удалёнными дублями:");
+        System.out.println(userList1);
+
+        //Задание №3 Отсортировать по возрасту и имени
+        userList1.sort(Comparator.comparing(o -> o.age));
+        System.out.println("Коллекция пользователей отсортированная по возрасту:");
+        for (User user : userList1) {
+            System.out.println(user);
+        }
+        System.out.println(userList1);
+
+        userList1.sort(Comparator.comparing(o -> o.name));
+        System.out.println("Коллекция пользователей отсортированная по имени:");
+        for (User user : userList1) {
+            System.out.println(user);
+        }
+        System.out.println(userList1);
+
+
+        //Задание №4 Вернуть самого старого пользователя
+        List<User> oldest1 = new ArrayList<>();
+        User oldest = null;
+        for (User user : userList1) {
+            if (oldest == null || user.getAge() > oldest.getAge()) {
+                oldest = user;
+                oldest1.clear();
+                oldest1.add(user);
+            } else if (user.getAge() == oldest.getAge()) {
+                oldest1.add(user);
+            }
+        }
+        System.out.println("Самый старый пользователь:");
+        System.out.println(oldest1);
+    }
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.Objects;
+
+public class User{
+    String name;
+    Integer age;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
